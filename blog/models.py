04021d27d41a97ea54165c5a6d9b793b5d3bcedf
Rebekah <rebekahzipporah@gmail.com>
2018-08-04 16:20:55 -0500

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -16,17 +16,11 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-from django.contrib import admin
-from .models import Post
 
-admin.site.register(Post)
-*.pyc
-*~
-__pycache__
-myvenv
-db.sqlite3
-/static
-.DS_Store
-        
+
+
+
+
+
 
 # Create your models here.
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Credef post_list(request):
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
